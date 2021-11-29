111222c9f8a9d103648e441b348b366b07e8beba
Пололин Сергей Александрович <spololin@sportmaster.ru>
2021-11-29 19:54:28 +0300

    Реализовал задачу 1.17

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+﻿<template>
+  <AppLayoutHeader />
+</template>
+
+<script>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader";
+
+export default {
+  name: "AppLayout",
+  components: {
+    AppLayoutHeader,
+  },
+};
+</script>
+
+<style></style>

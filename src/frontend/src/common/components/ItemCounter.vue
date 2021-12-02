ba39ad83ea0e7be585b0845f41e8869704a2fd13
Пололин Сергей Александрович <spololin@sportmaster.ru>
2021-12-02 20:59:17 +0300

    Создал общие компоненты

---
+++ src/frontend/src/common/components/ItemCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="counter counter--orange ingredients__counter">
+    <button
+      type="button"
+      class="counter__button counter__button--minus"
+      disabled
+    >
+      <span class="visually-hidden">Меньше</span>
+    </button>
+    <input type="text" name="counter" class="counter__input" value="0" />
+    <button type="button" class="counter__button counter__button--plus">
+      <span class="visually-hidden">Больше</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemCounter",
+};
+</script>
---
+++ src/frontend/src/common/components/RadioButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <input
+    type="radio"
+    :name="params.type"
+    :value="params.value"
+    :checked="params.checked"
+    class="visually-hidden"
+  />
+</template>
+
+<script>
+export default {
+  name: "RadioButton",
+  props: {
+    params: {
+      type: Object,
+      required: true,
+      validator: (v) =>
+        v.type.length && ["sauce", "dough", "diameter"].includes(v.type),
+    },
+  },
+};
+</script>

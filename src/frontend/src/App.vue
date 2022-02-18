4f7e3e035bffde10cc1b3915d0241b38aa416f87
Пололин Сергей Александрович <spololin@sportmaster.ru>
2022-02-18 23:43:59 +0300

    Сделал анимацию перетаскивания и router

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <AppLayout>
-      <router-view />
+      <transition
+        name="view"
+        appear
+        mode="out-in"
+        enter-active-class="animate-view"
+      >
+        <router-view />
+      </transition>
     </AppLayout>
   </div>
 </template>
@@ -22,4 +29,19 @@ export default {
 
 <style lang="scss">
 @import "~@/assets/scss/app";
+
+.animate-view {
+  animation: transformRotateY;
+  animation-duration: .7s;
+}
+
+@keyframes transformRotateY {
+  0% {
+    transform: rotateY(180deg);
+  }
+
+  to {
+    transform: rotateY(0);
+  }
+}
 </style>
